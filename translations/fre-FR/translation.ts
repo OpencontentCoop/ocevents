d6881ad503cdb02b7f62b264c7bfca79f4d1f246
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:40:05 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,511 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>ocevents/attribute</name>
+    <message>
+      <source>Recurrent event</source>
+      <translation>Événement récurrent</translation>
+    </message>
+    <message>
+      <source>Start</source>
+      <translation>Début</translation>
+    </message>
+    <message>
+      <source>Start Time</source>
+      <translation>Heure de début</translation>
+    </message>
+    <message>
+      <source>End</source>
+      <translation>Fin</translation>
+    </message>
+    <message>
+      <source>End Time</source>
+      <translation>Heure de fin</translation>
+    </message>
+    <message>
+      <source>Timezone</source>
+      <translation>Fuseau horaire</translation>
+    </message>
+    <message>
+      <source>Recurrence</source>
+      <translation>Récurrence</translation>
+    </message>
+    <message>
+      <source>None (run once)</source>
+      <translation>Aucune (événement unique)</translation>
+    </message>
+    <message>
+      <source>Daily</source>
+      <translation>Quotidienne</translation>
+    </message>
+    <message>
+      <source>Weekly</source>
+      <translation>Hebdomadaire</translation>
+    </message>
+    <message>
+      <source>Monthly</source>
+      <translation>Mensuelle</translation>
+    </message>
+    <message>
+      <source>every</source>
+      <translation>tous les</translation>
+    </message>
+    <message>
+      <source>Every</source>
+      <translation>Tous les</translation>
+    </message>
+    <message>
+      <source>Mon</source>
+      <translation>Lun</translation>
+    </message>
+    <message>
+      <source>Tue</source>
+      <translation>Mar</translation>
+    </message>
+    <message>
+      <source>Wed</source>
+      <translation>Mer</translation>
+    </message>
+    <message>
+      <source>Thu</source>
+      <translation>Jeu</translation>
+    </message>
+    <message>
+      <source>Fri</source>
+      <translation>Ven</translation>
+    </message>
+    <message>
+      <source>Sat</source>
+      <translation>Sam</translation>
+    </message>
+    <message>
+      <source>Sun</source>
+      <translation>Dim</translation>
+    </message>
+    <message>
+      <source>Mo</source>
+      <translation>Lu</translation>
+    </message>
+    <message>
+      <source>Tu</source>
+      <translation>Ma</translation>
+    </message>
+    <message>
+      <source>We</source>
+      <translation>Me</translation>
+    </message>
+    <message>
+      <source>Th</source>
+      <translation>Je</translation>
+    </message>
+    <message>
+      <source>Fr</source>
+      <translation>Ve</translation>
+    </message>
+    <message>
+      <source>Sa</source>
+      <translation>Sa</translation>
+    </message>
+    <message>
+      <source>Su</source>
+      <translation>Di</translation>
+    </message>
+    <message>
+      <source>on the</source>
+      <translation>le</translation>
+    </message>
+    <message>
+      <source>Monday</source>
+      <translation>Lundi</translation>
+    </message>
+    <message>
+      <source>Tuesday</source>
+      <translation>Mardi</translation>
+    </message>
+    <message>
+      <source>Wednesday</source>
+      <translation>Mercredi</translation>
+    </message>
+    <message>
+      <source>Thursday</source>
+      <translation>Jeudi</translation>
+    </message>
+    <message>
+      <source>Saturday</source>
+      <translation>Samedi</translation>
+    </message>
+    <message>
+      <source>Sunday</source>
+      <translation>Dimanche</translation>
+    </message>
+    <message>
+      <source>First</source>
+      <translation>Premier</translation>
+    </message>
+    <message>
+      <source>Second</source>
+      <translation>Deuxième</translation>
+    </message>
+    <message>
+      <source>Third</source>
+      <translation>Troisième</translation>
+    </message>
+    <message>
+      <source>Fourth</source>
+      <translation>Quatrième</translation>
+    </message>
+    <message>
+      <source>Last</source>
+      <translation>Dernier</translation>
+    </message>
+    <message>
+      <source>on</source>
+      <translation>le</translation>
+    </message>
+    <message>
+      <source>January</source>
+      <translation>Janvier</translation>
+    </message>
+    <message>
+      <source>February</source>
+      <translation>Février</translation>
+    </message>
+    <message>
+      <source>March</source>
+      <translation>Mars</translation>
+    </message>
+    <message>
+      <source>April</source>
+      <translation>Avril</translation>
+    </message>
+    <message>
+      <source>June</source>
+      <translation>Juin</translation>
+    </message>
+    <message>
+      <source>July</source>
+      <translation>Juillet</translation>
+    </message>
+    <message>
+      <source>August</source>
+      <translation>Août</translation>
+    </message>
+    <message>
+      <source>September</source>
+      <translation>Septembre</translation>
+    </message>
+    <message>
+      <source>October</source>
+      <translation>Octobre</translation>
+    </message>
+    <message>
+      <source>November</source>
+      <translation>Novembre</translation>
+    </message>
+    <message>
+      <source>December</source>
+      <translation>Décembre</translation>
+    </message>
+    <message>
+      <source>Jan</source>
+      <translation>Jan</translation>
+    </message>
+    <message>
+      <source>Feb</source>
+      <translation>Fév</translation>
+    </message>
+    <message>
+      <source>Mar</source>
+      <translation>Mar</translation>
+    </message>
+    <message>
+      <source>Apr</source>
+      <translation>Avr</translation>
+    </message>
+    <message>
+      <source>May</source>
+      <translation>Mai</translation>
+    </message>
+    <message>
+      <source>Jun</source>
+      <translation>J.n</translation>
+    </message>
+    <message>
+      <source>Jul</source>
+      <translation>J.t</translation>
+    </message>
+    <message>
+      <source>Aug</source>
+      <translation>Aoû</translation>
+    </message>
+    <message>
+      <source>Sep</source>
+      <translation>Sep</translation>
+    </message>
+    <message>
+      <source>Oct</source>
+      <translation>Oct</translation>
+    </message>
+    <message>
+      <source>Nov</source>
+      <translation>Nov</translation>
+    </message>
+    <message>
+      <source>Dec</source>
+      <translation>Déc</translation>
+    </message>
+    <message>
+      <source>End recurrence</source>
+      <translation>Fin de la récurrence</translation>
+    </message>
+    <message>
+      <source>Select week day</source>
+      <translation>Sélectionner le jour de la semaine</translation>
+    </message>
+    <message>
+      <source>Never</source>
+      <translation>Jamais</translation>
+    </message>
+    <message>
+      <source>After</source>
+      <translation>Après</translation>
+    </message>
+    <message>
+      <source>On date</source>
+      <translation>Le</translation>
+    </message>
+    <message>
+      <source>End After</source>
+      <translation>Fin après</translation>
+    </message>
+    <message>
+      <source>occurance(s)</source>
+      <translation>récurrence(s)</translation>
+    </message>
+    <message>
+      <source>12:00 AM</source>
+      <translation>00 H 00</translation>
+    </message>
+    <message>
+      <source>12:30 AM</source>
+      <translation>00 H 30</translation>
+    </message>
+    <message>
+      <source>1:00 AM</source>
+      <translation>01 H 00</translation>
+    </message>
+    <message>
+      <source>1:30 AM</source>
+      <translation>01 H 30</translation>
+    </message>
+    <message>
+      <source>2:00 AM</source>
+      <translation>02 H 00</translation>
+    </message>
+    <message>
+      <source>2:30 AM</source>
+      <translation>02 H 30</translation>
+    </message>
+    <message>
+      <source>3:00 AM</source>
+      <translation>03 H 00</translation>
+    </message>
+    <message>
+      <source>3:30 AM</source>
+      <translation>03 H 30</translation>
+    </message>
+    <message>
+      <source>4:00 AM</source>
+      <translation>04 H 00</translation>
+    </message>
+    <message>
+      <source>4:30 AM</source>
+      <translation>04 H 30</translation>
+    </message>
+    <message>
+      <source>5:00 AM</source>
+      <translation>05 H 00</translation>
+    </message>
+    <message>
+      <source>5:30 AM</source>
+      <translation>05 H 30</translation>
+    </message>
+    <message>
+      <source>6:00 AM</source>
+      <translation>06 H 00</translation>
+    </message>
+    <message>
+      <source>6:30 AM</source>
+      <translation>06 H 30</translation>
+    </message>
+    <message>
+      <source>7:00 AM</source>
+      <translation>07 H 00</translation>
+    </message>
+    <message>
+      <source>7:30 AM</source>
+      <translation>07 H 30</translation>
+    </message>
+    <message>
+      <source>8:00 AM</source>
+      <translation>08 H 00</translation>
+    </message>
+    <message>
+      <source>8:30 AM</source>
+      <translation>08 H 30</translation>
+    </message>
+    <message>
+      <source>9:00 AM</source>
+      <translation>09 H 00</translation>
+    </message>
+    <message>
+      <source>9:30 AM</source>
+      <translation>09 H 30</translation>
+    </message>
+    <message>
+      <source>10:00 AM</source>
+      <translation>10 H 00</translation>
+    </message>
+    <message>
+      <source>10:30 AM</source>
+      <translation>10 H 30</translation>
+    </message>
+    <message>
+      <source>11:00 AM</source>
+      <translation>11 H 00</translation>
+    </message>
+    <message>
+      <source>11:30 AM</source>
+      <translation>11 H 30</translation>
+    </message>
+    <message>
+      <source>12:00 PM</source>
+      <translation>12 H 00</translation>
+    </message>
+    <message>
+      <source>12:30 PM</source>
+      <translation>12 H 30</translation>
+    </message>
+    <message>
+      <source>1:00 PM</source>
+      <translation>13 H 00</translation>
+    </message>
+    <message>
+      <source>1:30 PM</source>
+      <translation>13 H 30</translation>
+    </message>
+    <message>
+      <source>2:00 PM</source>
+      <translation>14 H 00</translation>
+    </message>
+    <message>
+      <source>2:30 PM</source>
+      <translation>14 H 30</translation>
+    </message>
+    <message>
+      <source>3:00 PM</source>
+      <translation>15 H 00</translation>
+    </message>
+    <message>
+      <source>3:30 PM</source>
+      <translation>15 H 30</translation>
+    </message>
+    <message>
+      <source>4:00 PM</source>
+      <translation>16 H 00</translation>
+    </message>
+    <message>
+      <source>4:30 PM</source>
+      <translation>16 H 30</translation>
+    </message>
+    <message>
+      <source>5:00 PM</source>
+      <translation>17 H 00</translation>
+    </message>
+    <message>
+      <source>5:30 PM</source>
+      <translation>17 H 30</translation>
+    </message>
+    <message>
+      <source>6:00 PM</source>
+      <translation>18 H 00</translation>
+    </message>
+    <message>
+      <source>6:30 PM</source>
+      <translation>18 H 30</translation>
+    </message>
+    <message>
+      <source>7:00 PM</source>
+      <translation>19 H 00</translation>
+    </message>
+    <message>
+      <source>7:30 PM</source>
+      <translation>19 H 30</translation>
+    </message>
+    <message>
+      <source>8:00 PM</source>
+      <translation>20 H 00</translation>
+    </message>
+    <message>
+      <source>8:30 PM</source>
+      <translation>20 H 30</translation>
+    </message>
+    <message>
+      <source>9:00 PM</source>
+      <translation>21 H 00</translation>
+    </message>
+    <message>
+      <source>9:30 PM</source>
+      <translation>21 H 30</translation>
+    </message>
+    <message>
+      <source>10:00 PM</source>
+      <translation>22 H 00</translation>
+    </message>
+    <message>
+      <source>10:30 PM</source>
+      <translation>22 H 30</translation>
+    </message>
+    <message>
+      <source>11:00 PM</source>
+      <translation>23 H 00</translation>
+    </message>
+    <message>
+      <source>11:30 PM</source>
+      <translation>23 H 30</translation>
+    </message>
+    <message>
+      <source>Month</source>
+      <translation>Mois</translation>
+    </message>
+    <message>
+      <source>Year</source>
+      <translation>Année</translation>
+    </message>
+    <message>
+      <source>Show recurrences calendar</source>
+      <translation>Ouvrir le calendrier des récurrences</translation>
+    </message>
+    <message>
+      <source>Recurrences calendar</source>
+      <translation>Calendrier des récurrences</translation>
+    </message>
+    <message>
+      <source>Modify event</source>
+      <translation>Modifier l’événement</translation>
+    </message>
+    <message>
+      <source>Delete</source>
+      <translation>Supprimer</translation>
+    </message>
+    <message>
+      <source>Store</source>
+      <translation>Enregistrer</translation>
+    </message>
+    <message>
+      <source>Recurrence text</source>
+      <translation>Texte des récurrences</translation>
+    </message>
+  </context>
+</TS>
